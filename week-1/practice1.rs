3df9ce4f337adc694925d1863c33292091f5d4e0
unknown <Chidera.Onodingene@pau.edu.ng>
2023-10-21 20:05:53 +0100

    (My first commit)

---
+++ week-1/practice1.rs
@@ -0,0 +1,19 @@
+fn main(){ println!("Welcome to COS 101!");
+
+    println!("The couse learning outcome is as follows:");
+
+    println!("1. Distinguish between computer and coputer programming,/n
+              2. Develop some techniques in computer science,/n
+              3. Understand the different areas od study in computer science,/science
+              4. Conversant with application of computer science,/and
+              5. Navigate the carrer prospects in computer science./science
+              6. Conversant with computer programming concepts." );
+
+
+
+
+
+
+
+}
+   
